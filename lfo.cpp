9893c5de5b4cdd9d5a60e24696946858c71649a9
Antti Lappalainen <antti@prototyper.tech>
2021-02-19 03:40:10 +0200

    Init

---
+++ lfo.cpp
@@ -0,0 +1,10 @@
+//b----------------------------------------
+// - Declare an output model with a Low Freq Oscillatior,
+// - That modulates a phaser 
+// - The modulated phaser is summed in the input signal,
+// - So the output should be quite interesing
+//b----------------------------------------
+
+#include <lv2plugin.hpp>
+
+
